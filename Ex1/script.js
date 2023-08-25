0e6df0b7c485e69c5d937be1d727a3c94a97b4bc
Rafael Silva <76886825+Rafael-Nunes-Silva@users.noreply.github.com>
2023-08-25 14:06:20 -0300

    Atualização da atividade 1

--- Ex1/script.js
+++ Ex1/script.js
@@ -1,35 +1,29 @@
-const nome = prompt("Informe seu nome:");
-const altura = parseInt(prompt("Informe sua altura (cm):")) / 100;
-const peso = parseFloat(prompt("Informe seu peso (Kg):"));
+document.getElementById("botaoCalcular").addEventListener(
+    "click",
+    function(){
+        const nome = document.getElementById("nome").value;
+        const altura = parseInt(document.getElementById("altura").value) / 100;
+        const peso = parseFloat(document.getElementById("peso").value);
 
-const M = (peso / (altura ** 2)).toFixed(2);
+        const M = (peso / (altura ** 2)).toFixed(2);
 
-let classificacao = "";
+        let classificacao = "Baixo peso muito grave";
 
-if (M > 40){
-    classificacao = "Obesidade grau III";
-}
-else if (M > 35){
-    classificacao = "Obesidade grau II";
-}
-else if (M > 30){
-    classificacao = "Obesidade grau I";
-}
-else if (M > 25){
-    classificacao = "Sobrepeso";
-}
-else if (M > 18.5){
-    classificacao = "Peso normal";
-}
-else if (M > 17){
-    classificacao = "Baixo peso";
-}
-else if (M > 16){
-    classificacao = "Baixo peso grave";
-}
-else {
-    classificacao = "Baixo peso muito grave";
-}
-
-document.getElementById("resultado").innerText = `${nome} possui índice de massa corporal igual a ${M}, sendo classificado como: ${classificacao}.`;
-// alert(`${nome} possui índice de massa corporal igual a ${M}, sendo classificado como: ${classificacao}.`);
+        if (M > 40)
+            classificacao = "Obesidade grau III";
+        else if (M > 35)
+            classificacao = "Obesidade grau II";
+        else if (M > 30)
+            classificacao = "Obesidade grau I";
+        else if (M > 25)
+            classificacao = "Sobrepeso";
+        else if (M > 18.5)
+            classificacao = "Peso normal";
+        else if (M > 17)
+            classificacao = "Baixo peso";
+        else if (M > 16)
+            classificacao = "Baixo peso grave";
+        
+        document.getElementById("resultado").innerText = `${nome} possui índice de massa corporal igual a ${M}, sendo classificado como: ${classificacao}.`;
+    }
+);
