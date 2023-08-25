8b7cdbf94caa521b75c63b92e5ddad475ed8c5c3
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-08-25 20:45:07 -0300

    Exercicío 4

---
+++ Ex4/script.js
@@ -0,0 +1,51 @@
+window.onload = function(){
+    const selectRegiao = document.getElementById("regiao");
+
+    for(let i = 0; i < regioes.length; i++){
+        const regiao = document.createElement("option");
+        regiao.innerText = regioes[i].nome;
+        regiao.value = i;
+        selectRegiao.appendChild(regiao);
+    }
+
+    selectRegiao.selectedIndex = 0;
+}
+
+document.getElementById("botaoCalcular").addEventListener(
+    "click",
+    function(){
+        const codigo = parseInt(document.getElementById("codigo").value);
+        const horas = parseInt(document.getElementById("horas").value);
+
+        const turnoElement = document.getElementById("turno");
+        const turno = turnoElement[turnoElement.selectedIndex].value;
+
+        const categoriaElement = document.getElementById("categoria");
+        const categoria = categoriaElement[categoriaElement.selectedIndex].value;
+
+        const salarioMinimo = document.getElementById("salario").value;
+
+        let valorHoraTrabalhada = 0;
+        switch(categoria){
+            case "G":
+                if(turno === "M" || turno === "V")
+                    valorHoraTrabalhada = 0.04 * salarioMinimo;
+                break;
+            case "F":
+                if(turno === "N")
+                    valorHoraTrabalhada = 0.02 * salarioMinimo;
+                else if(turno === "M" || turno === "V")
+                    valorHoraTrabalhada = 0.01 * salarioMinimo;
+                break;
+        }
+        
+        const salarioInicial = valorHoraTrabalhada * horas;
+        let valorAuxilioAlimentacao = salarioInicial * 0.25;
+        if(salarioInicial > 1200)
+            valorAuxilioAlimentacao = salarioInicial * 0.2;
+        else if(salarioInicial > 800)
+            valorAuxilioAlimentacao = salarioInicial * 0.15;
+
+        document.getElementById("resultado").innerText = `Código: ${codigo}\nHoras | Valor/Hora: ${horas} | ${valorHoraTrabalhada}\nSalário Inicial | Auxílio Alimentação: ${salarioInicial} | ${valorAuxilioAlimentacao}\nSalário Final: ${salarioInicial + valorAuxilioAlimentacao}`;
+    }
+);
